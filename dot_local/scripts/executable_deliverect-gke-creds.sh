1f8bc8c1ac295c6b25872f2e291479816cb5bcf0
Novian Deny <anggerdeni@gmail.com>
2025-10-08 09:36:06 +0700

    feat: kubectl deliverect stuff

--- dot_local/scripts/executable_deliverect-gke-creds.sh
+++ dot_local/scripts/executable_deliverect-gke-creds.sh
@@ -3,4 +3,6 @@ gcloud container clusters get-credentials dev --zone europe-west1-b --project de
 gcloud container clusters get-credentials testing --zone europe-west1-b --project deliverect-testing
 gcloud container clusters get-credentials staging --zone europe-west1-b --project deliverect-staging
 gcloud container clusters get-credentials monitoring --zone europe-west1-b --project deliverect-staging
+gcloud container clusters get-credentials ypodomi-nonprod --region europe-west1 --project monitoring-nonprod-710788
+gcloud container clusters get-credentials istio-test --region europe-west1 --project localtests-344116
 # gcloud container clusters get-credentials production --zone europe-west1-b --project deliverect-production
