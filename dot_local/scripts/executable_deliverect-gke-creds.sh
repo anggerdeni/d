a80bf806b86c70c13445d3f7e930255d078c116d
Novian Deny <anggerdeni@gmail.com>
2025-11-10 10:00:25 +0700

    chore: add gke auth to deliverect ypodomi production

--- dot_local/scripts/executable_deliverect-gke-creds.sh
+++ dot_local/scripts/executable_deliverect-gke-creds.sh
@@ -5,4 +5,5 @@ gcloud container clusters get-credentials staging --zone europe-west1-b --projec
 gcloud container clusters get-credentials monitoring --zone europe-west1-b --project deliverect-staging
 gcloud container clusters get-credentials ypodomi-nonprod --region europe-west1 --project monitoring-nonprod-710788
 gcloud container clusters get-credentials istio-test --region europe-west1 --project localtests-344116
+gcloud container clusters get-credentials ypodomi-production --region europe-west1 --project monitoring-prod-359018
 # gcloud container clusters get-credentials production --zone europe-west1-b --project deliverect-production
